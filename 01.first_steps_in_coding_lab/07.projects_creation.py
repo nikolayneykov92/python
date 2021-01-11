8c89613a96855123e3cdf3b796538d113af4c742
Nikolay Neykov <nikolay.neykov@jpmchase.com>
2021-01-11 11:29:54 +0200

    added first steps in coding lab solutions

---
+++ 01.first_steps_in_coding_lab/07.projects_creation.py
@@ -0,0 +1,6 @@
+architect_name = input()
+projects_count = int(input())
+
+print(
+    f'The architect {architect_name} will need {projects_count*3} hours to complete {projects_count} project/s.'
+)
---
+++ 01.first_steps_in_coding_lab/08.pet_shop.py
@@ -0,0 +1,5 @@
+dogs_count = int(input())
+other_animals_count = int(input())
+total_sum = dogs_count * 2.5 + other_animals_count * 4
+
+print(f'{total_sum} lv.')
